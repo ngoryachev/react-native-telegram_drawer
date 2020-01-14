4f1e06a3a67bfa082477673eb32ff4d4b443b239
Nick Goriachev <ngoryacheff@gmail.com>
2020-01-14 11:25:08 +0200

    initial rn infra

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View style={{ backgroundColor: 'green', flex: 1 }}>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
