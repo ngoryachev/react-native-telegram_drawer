bf3d4a7fa985c1286f5233a48e051e4fd3e1c172
Nick Goriachev <ngoryacheff@gmail.com>
2020-01-20 07:59:30 +0200

    refactored ChatScreen

---
+++ src/screens/ChatScreen.tsx
@@ -0,0 +1,102 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import IconEvil from 'react-native-vector-icons/EvilIcons';
+import CameraRoll, {
+  PhotoIdentifier,
+  PhotoIdentifiersPage,
+} from '@react-native-community/cameraroll';
+import PickPhotoDrawer from '../..//src/components/PickPhotoDrawer';
+import {Spacer} from '../../uiUtils';
+import {Row} from '../../components';
+import sizes from '../../src/styles/sizes';
+import {READ_STORAGE, requestPermission} from '../utils/permissions';
+
+const ChatScreen = () => {
+  const drawerEl = useRef<PickPhotoDrawer>(null);
+  const [photos, setPhotos] = useState<PhotoIdentifier[]>([]);
+  useEffect(() => {
+    requestPermission(READ_STORAGE)
+      .then(() =>
+        CameraRoll.getPhotos({
+          first: 60,
+          assetType: 'Photos',
+        }),
+      )
+      .then((r: PhotoIdentifiersPage) => {
+        setPhotos(r.edges);
+      });
+  }, []);
+
+  const keyboardBehaviour: any = Platform.select({
+    ios: 'padding',
+    android: undefined,
+  });
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.containerStyle}
+      behavior={keyboardBehaviour}>
+      <TouchableOpacity
+        activeOpacity={1}
+        style={styles.fillerStyle}
+        onPress={() => drawerEl.current!.startDisappearAnimation()}
+      />
+      <PickPhotoDrawer data={photos} ref={drawerEl}>
+        <Row alignCenter style={styles.inputContainer}>
+          <TouchableOpacity
+            onPress={() => {
+              if (drawerEl.current) {
+                drawerEl.current.startAppearAnimation();
+                Keyboard.dismiss();
+              }
+            }}>
+            <Icon name="paperclip" size={21} color="#958FAA" />
+          </TouchableOpacity>
+          <Spacer w={31} />
+          <TextInput
+            placeholderTextColor="#958FAA"
+            placeholder="Сообщение..."
+            style={styles.inputStyle}
+          />
+          <IconEvil name="sc-telegram" size={25} color="#958FAA" />
+        </Row>
+      </PickPhotoDrawer>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputStyle: {
+    fontSize: 16,
+    color: '#3D3737',
+    flex: 1,
+    fontFamily: 'Source Sans Pro',
+  },
+  inputContainer: {
+    height: 63,
+    paddingHorizontal: sizes.padding,
+    backgroundColor: 'white',
+  },
+  fillerStyle: {flex: 1, backgroundColor: '#F2F7FA'},
+  containerStyle: {backgroundColor: 'gray', flex: 1},
+});
+
+export default ChatScreen;
